a551b256f3574b6d5c0db09a78778cec5ea6c62b
Neeraja824 <lankaneeraja824@gmail.com>
2026-01-29 18:58:37 +0530

    first

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from 'react';
+import { StyleSheet, Text,Image,TouchableOpacity,Button} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import MyImage from './assets/favicon.png';
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image source={MyImage} style={styles.Image}/>
+      <TouchableOpacity>
+        <Text style={styles.mytext}>Neeraja</Text>
+      </TouchableOpacity>
+      <Button title="Click Me"></Button>
+    </SafeAreaView>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  mytext: {
+    color: 'blue',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+   Image:{
+    width: 100,
+    height: 100,
+  }
 });
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Annapurna(Maamuu) </Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
