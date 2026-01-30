2d46f9a8b5dcc44e43686e6ea9caad5dc7510eb9
Neeraja824 <lankaneeraja824@gmail.com>
2026-01-30 18:51:00 +0530

    first

--- App.js
+++ App.js
@@ -1,38 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text,Image,TouchableOpacity,Button} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import MyImage from './assets/favicon.png';
+// import React from 'react';
+// import { StyleSheet, Text,Image,TouchableOpacity,Button} from 'react-native';
+// import { SafeAreaView } from 'react-native-safe-area-context';
+// import MyImage from './assets/favicon.png';
+// const App = () => {
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <Image source={MyImage} style={styles.Image}/>
+//       <TouchableOpacity>
+//         <Text style={styles.mytext}>Neeraja</Text>
+//       </TouchableOpacity>
+//       <Button title="Click Me"></Button>
+//     </SafeAreaView>
+//   );
+// };
+
+// export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   mytext: {
+//     color: 'blue',
+//     fontSize: 30,
+//     fontWeight: 'bold',
+//   },
+//    Image:{
+//     width: 100,
+//     height: 100,
+//   }
+// });
+
+
+import react from "react";
+import { View, Text, Button, TouchableOpacity, Image, StyleSheet } from "react-native";
+import MyImage from "./assets/favicon.png"
+import { StatusBar } from "react-native";
+import Component from "./Component";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Image source={MyImage} style={styles.Image}/>
-      <TouchableOpacity>
-        <Text style={styles.mytext}>Neeraja</Text>
-      </TouchableOpacity>
-      <Button title="Click Me"></Button>
-    </SafeAreaView>
-  );
-};
+    <>
+      <SafeAreaProvider>
+        <Component />
+      </SafeAreaProvider>
+    </>
+  )
+}
 
 export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  mytext: {
-    color: 'blue',
-    fontSize: 30,
-    fontWeight: 'bold',
-  },
-   Image:{
-    width: 100,
-    height: 100,
-  }
-});
 
+
+
+const Mystyles = StyleSheet.create({
+    MystatusBar:{
+        backgroundColor:"green"
+    }
+})
 // export default function App() {
 //   return (
 //     <View style={styles.container}>
