c0a17d031b52e166fea5eeb51ca6ed9f6c9b1510
Neeraja824 <lankaneeraja824@gmail.com>
2026-02-11 20:15:00 +0530

    files

--- App.js
+++ App.js
@@ -34,32 +34,29 @@
 // });
 
 
-import react from "react";
-import { View, Text, Button, TouchableOpacity, Image, StyleSheet } from "react-native";
-import MyImage from "./assets/favicon.png"
-import { StatusBar } from "react-native";
-import Component from "./Component";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-const App = () => {
-  return (
-    <>
-      <SafeAreaProvider>
-        <Component />
-      </SafeAreaProvider>
-    </>
-  )
-}
+// import react from "react";
+// import { View, Text, Button, TouchableOpacity, Image, StyleSheet } from "react-native";
+// import MyImage from "./assets/favicon.png"
+// import { StatusBar } from "react-native";
+// import Component from "./Component";
+// import { SafeAreaProvider } from "react-native-safe-area-context";
+// const App = () => {
+//   return (
+//     <>
+//       <SafeAreaProvider>
+//         <Component />
+//       </SafeAreaProvider>
+//     </>
+//   )
+// }
 
-export default App;
+// export default App;
 
-
-
-
-const Mystyles = StyleSheet.create({
-    MystatusBar:{
-        backgroundColor:"green"
-    }
-})
+// const Mystyles = StyleSheet.create({
+//     MystatusBar:{
+//         backgroundColor:"green"
+//     }
+// })
 // export default function App() {
 //   return (
 //     <View style={styles.container}>
@@ -77,3 +74,61 @@ const Mystyles = StyleSheet.create({
 //     justifyContent: 'center',
 //   },
 // });
+
+
+import react from "react";
+import { View, Text, Button, TouchableOpacity, Image, StyleSheet, FlatList } from "react-native";
+import Image_1 from "./assets/favicon.png"
+import Image_2 from "./assets/adaptive-icon.png"
+import Image_3 from "./assets/icon.png"
+import Image_4 from "./assets/splash-icon.png"
+import { StatusBar } from "react-native";
+import Component from "./Component";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+const MyData = [
+  { id: '1', name: 'Aditya', image: Image_1 },
+  { id: '2', name: 'Neeru', image: Image_2 },
+  { id: '3', name: 'Uma', image: Image_3 },
+  { id: '4', name: 'Akshu', image: Image_4 },
+];  
+
+const App = () => {
+  return (
+    <>
+      <FlatList 
+        data={MyData}
+        renderItem={({ item }) => (
+        <View style={Mystyles.parent}>
+            <Image source={item.image} style={Mystyles.image} />
+            <Text style={Mystyles.text}>{item.name}</Text>
+        </View>
+        )}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item)=>item.id}
+      />
+    </>
+  )
+}
+
+export default App;
+const Mystyles = StyleSheet.create({
+  parent:{
+    flexDirection:"row",
+    alignItems:"center",
+    margin:10,
+    backgroundColor:"lightgray",
+    padding:10,
+    borderRadius:10
+  },
+  image:{
+    width:50,
+    height:50,
+    borderRadius:25,
+    marginRight:10
+  },
+  text:{
+    fontSize:18,
+    fontWeight:"bold"
+  }
+})
--- Component.jsx
+++ Component.jsx
@@ -1,9 +1,13 @@
 import react from "react";
-import { View ,StyleSheet,StatusBar,Text} from "react-native";
+import { View ,StyleSheet,StatusBar,Text,Image} from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { ActivityIndicator,Badge,Button,Chip,TextInput,Icon } from "react-native/types_generated/index";
 const Component = () =>{
     const Results = useSafeAreaInsets();
-    console.log(Results)
+    const Fun=()=>{
+        console.log("Button is Pressed",new Date())
+    }
+    // console.log(Results)
     return(
         <>
             <StatusBar barStyle="light-content" hidden />
