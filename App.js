a78e60b994def21dcd22cc9ab2b9624cd6b82446
Neeraja824 <lankaneeraja824@gmail.com>
2026-03-11 10:00:45 +0530

    files

--- App.js
+++ App.js
@@ -247,42 +247,61 @@
 //   }
 // });
 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Icon } from 'react-native-paper';
-import Home from './Home';
-import Contact from './Contact';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import {Icon} from 'react-native-paper';
-import { createStackNavigator } from '@react-navigation/stack';
-const Stack = createNativeStackNavigator()
-export default function App() {
-  return (
-   <NavigationContainer>
-    <Stack.Navigator
-      screenOptions={{
-        // title:"HAI"
-        // headerShown:false
-        // headerStyle:{
-        //   backgroundColor:"red"
-        // }
-        // headerTintColor:"red"
-        // headerTitleStyle:{
-        //   fontSize:26
-        // }
-        headerTitleAlign:"center",
-        headerRight:()=> <Icon source={"bell"} size={24}/>,
-        // headerLeft:() => <Icon source={"home"} size={24}/>,
-        headerBackButtonDisplayMode:"minimal",
-        animation:"fade"
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// import { Icon } from 'react-native-paper';
+// import Home from './Home';
+// import Contact from './Contact';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// // import {Icon} from 'react-native-paper';
+// import { createStackNavigator } from '@react-navigation/stack';
+// const Stack = createNativeStackNavigator()
+// export default function App() {
+//   return (
+//    <NavigationContainer>
+//     <Stack.Navigator
+//       screenOptions={{
+//         // title:"HAI"
+//         // headerShown:false
+//         // headerStyle:{
+//         //   backgroundColor:"red"
+//         // }
+//         // headerTintColor:"red"
+//         // headerTitleStyle:{
+//         //   fontSize:26
+//         // }
+//         headerTitleAlign:"center",
+//         headerRight:()=> <Icon source={"bell"} size={24}/>,
+//         // headerLeft:() => <Icon source={"home"} size={24}/>,
+//         headerBackButtonDisplayMode:"minimal",
+//         animation:"fade"
 
-      }}
-    >
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Contact" component={Contact} />
-    </Stack.Navigator>
-   </NavigationContainer>
+//       }}
+//     >
+//       <Stack.Screen name="Home" component={Home} />
+//       <Stack.Screen name="Contact" component={Contact} />
+//     </Stack.Navigator>
+//    </NavigationContainer>
+//   );
+// }
+
+import React, { useState } from "react";
+import { View,Text } from "react-native";
+import { Button } from "react-native-paper";
+import * as LocalAuthentication from "expo-local-authentication"
+export default function App() {
+  const Open = async() =>{
+    const status = await LocalAuthentication.hasHardwareAsync()
+    console.log("hardware",status)
+    const isEnrolled = await LocalAuthentication.isEnrolledAsync()
+    console.log("Enrolled",isEnrolled)
+    const result = await LocalAuthentication.authenticateAsync()
+    console.log(result)
+  }
+  return (
+    <View style={{ marginTop: 100 }}>
+      <Button onPress={Open}>Click</Button>
+    </View>
   );
 }
-
